a721a9dd45a2aa30a7e747317fca91cc1af9fcf3
Anuwat <admin@unknownclub.net>
2022-08-27 00:00:56 +0700

    fix: fix bottom nav bugs

--- lib/screen/bottom_bar.dart
+++ lib/screen/bottom_bar.dart
@@ -36,6 +36,7 @@ class _BottomBarState extends State<BottomBar> {
       ),
       body: Center(child: _widgetOptions[_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         elevation: 10,
         showSelectedLabels: false,
@@ -49,15 +50,15 @@ class _BottomBarState extends State<BottomBar> {
             label: 'Home'),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_search_regular), 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_regular),            
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),            
             label: 'Search'),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular), 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),            
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),            
             label: 'Ticket'),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_person_regular), 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_regular),  
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),  
             label: 'Profile'),
         ],
       ),
